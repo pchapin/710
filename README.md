6cabba25fee1e9c93630398b76b2ed8c7828cb49
Peter Chapin <spicacality@gmail.com>
2016-06-07 15:50:29 -0400

    Initial commit from existing code base.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# 710
-A stellar encounter simulator
+
+710
+===
+
+This project is about analyzing encounters between the solar system and objects coming from
+outside the solar system. Monte Carlo methods are used to evaluate, specifically, the risk to
+Earth of collisions with extrasolar objects.
+
+The name of this project comes from [Gliese 710](http://en.wikipedia.org/wiki/Gliese_710), a
+star that will pass closely to the sun in 1.4 million years. When that occurs Gliese 710 may
+disrupt the Oort cloud surrounding the sun and cause cometary objects to enter the inner solar
+system. However, it is also possible that the sun will pass through an Oort cloud (presumably)
+surrounding Gliese 710. The purpose of this project is to explore the implications of that
+second kind of encounter. Will the sun capture any extrasolar comets currently orbiting Gliese
+710? What is the likelihood that any of those objects will eventually collide with the Earth?
+This project is about conducting numerical experiments to address these questions, not just for
+Gliese 710 but for any similar kind of encounter.
+
+Peter Chapin  
+spicacality@gmail.com
---
+++ planets.jl
@@ -0,0 +1,39 @@
+
+# Define an immutable structure for holding information about the planets.
+immutable PlanetData
+    radius   :: Float64
+    mass     :: Float64
+    distance :: Float64
+    period   :: Float64
+end
+
+earth_mass = 5.972E+24  # Mass in kilograms.
+earth_distance = 1.496E+11  # Distance from sun in meters.
+earth_period = 3600.0 * 24.0 * 365.25  # Period in seconds.
+
+# Open the planets file and read it one line at a time.
+planet_data = Dict{String, PlanetData}()
+planet_file = open("planets.txt")
+line_iterator = eachline(planet_file)
+for line = line_iterator
+
+    # TODO: Deal with comments a little more gracefully.
+    if line[1] != '#'
+        fields = split(line)
+        if length(fields) != 5
+            # TODO: Give the user a bit more help about badly formatted input lines.
+            print("Bad line found in file!\n")
+        else
+            planet_data[fields[1]] =
+                PlanetData(
+                    float(fields[2]) / 2.0,
+                    float(fields[3]) * earth_mass,
+                    float(fields[4]) * earth_distance,
+                    float(fields[5]) * earth_period)
+        end
+    end
+end
+close(planet_file)
+
+# Display the results.
+print(planet_data)
